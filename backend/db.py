f46cec59db318cc2d030c529302dc17e9c8096de
Alexandra Marzuqa Giacaman <amarzuqa@gmail.com>
2025-03-29 14:18:16 -0300

    enhance type safety, add delete functionality, and update dependencies

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 from sqlalchemy import String, create_engine
 
+
 class Base(DeclarativeBase):
     pass
+
 
 class Scheduler(Base):
     __tablename__ = "scheduler"
@@ -11,9 +13,10 @@ class Scheduler(Base):
     time: Mapped[int]
     name: Mapped[str] = mapped_column(String(20))
 
+
 engine = create_engine("sqlite:///scheduler.db")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
-    print('db succesfully created!!')
+    print("db succesfully created!!")
